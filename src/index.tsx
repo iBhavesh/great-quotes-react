406d83f33d80d48c24d31c882458ed47813fbc7e
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-05-16 13:39:06 +0000

    remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { store } from "./store/store";
-import { Provider } from "react-redux";
 import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router basename="/">
-        <App />
-      </Router>
-    </Provider>
+    <Router basename="/">
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
