e17654c66e272388ab9533794702dd09c7abb237
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-05-16 11:17:11 +0000

    deploy to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, Route, Switch, useRouteMatch } from "react-router";
+import { Redirect, Route, Switch } from "react-router";
 import "./App.css";
 import Layout from "./components/layout/Layout";
 import NewQuote from "./pages/NewQuote";
@@ -8,24 +8,19 @@ import Quotes from "./pages/AllQuotes";
 import NotFound from "./pages/NotFound";
 
 function App() {
-  let { url } = useRouteMatch();
-  if (url === "/") {
-    url = "";
-  }
-
   return (
     <Layout>
       <Switch>
-        <Route exact path={url + "/"}>
-          <Redirect to={url + "quotes"} />
+        <Route exact path="/">
+          <Redirect to="quotes" />
         </Route>
-        <Route exact path={url + "/quotes"}>
+        <Route exact path="/quotes">
           <Quotes />
         </Route>
-        <Route path={url + "/quotes/:quoteId"}>
+        <Route path="/quotes/:quoteId">
           <QuoteDetail />
         </Route>
-        <Route path={url + "/new-quote"}>
+        <Route path="/new-quote">
           <NewQuote />
         </Route>
         <Route path="*">
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
